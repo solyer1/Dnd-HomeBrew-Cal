6415767bfaa47ef4fd7cae70848f0e9a16e13d48
Soly <rama81te@gmail.com>
2026-07-13 16:16:39 +0700

    feat: in-app user guide modal (EN + TH, 7 slides)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ import React, { useState } from 'react';
 import { DamageCalculator } from '@/components/calculator/DamageCalculator';
 import { DiceRoller } from '@/components/dice/DiceRoller';
 import { SettingsPanel } from '@/components/ui/SettingsPanel';
+import { GuideModal } from '@/components/ui/GuideModal';
 import { useAppContext } from '@/context/AppContext';
 import { useTranslation } from '@/hooks/useTranslation';
 
@@ -66,6 +67,9 @@ export default function Home() {
 
       {/* Settings panel */}
       <SettingsPanel />
+
+      {/* Guide modal */}
+      <GuideModal />
 
       {/* Main content */}
       <div className="relative z-10 min-h-screen flex flex-col">
